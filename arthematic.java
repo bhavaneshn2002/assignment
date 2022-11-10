899133283c3ce23361c5906c6e41c63f4018c5f7
bhavaneshn2002 <114063882+bhavaneshn2002@users.noreply.github.com>
2022-11-10 20:39:12 +0530

    Add files via upload

---
+++ arthematic.java
@@ -0,0 +1,15 @@
+public class arthematic
+{
+public static void main(String[] args)
+{
+int num1 = 20, num2 = 15, res;
+res = num1 + num2;
+System.out.println("addition is=" + res);
+res = num1 - num2;
+System.out.println("subtraction is=" + res);
+res = num1 * num2;
+System.out.println("multiplication is=" + res);
+res = num1 / num2;
+System.out.println("division is=" + res);
+}
+}
